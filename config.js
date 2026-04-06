aabe533656e7744319701a6431de326896effb10
pc9839a-lgtm <pc9839a@naver.com>
2026-04-06 14:20:14 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwYbilq7hPQ8ZoJmzIvRBJf_Q5ip9-uzf_e-HV6QwBeMl9B10UWBy_VFNFzmCIBNWM/exec',
+  useMockOnly: false,
+  submitTimeout: 30000
+};
