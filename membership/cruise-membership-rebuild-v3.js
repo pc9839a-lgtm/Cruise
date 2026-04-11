bd7a2a82fb29839bf66bfe9fed2f977a47e11307
pc9839a-lgtm <pc9839a@naver.com>
2026-04-11 11:01:56 +0900

    Update cruise-membership-rebuild-v3.js

--- membership/cruise-membership-rebuild-v3.js
+++ membership/cruise-membership-rebuild-v3.js
@@ -56,7 +56,7 @@ const state = {
   agentCode: ''
 };
 
-const AGENT_API_URL = (window.APP_CONFIG && window.APP_CONFIG.apiUrl) || '';
+const AGENT_API_URL = 'https://script.google.com/macros/s/AKfycbxmpUiHPpZObUpjzV8R-cV32UjB-Q64ST_MyCi6xixSB9dHBhxdpdeVTbDV4gwYXv0/exec';
 
 
 function formatUsd(value) {
