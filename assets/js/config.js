8ad4f3e65ac5efcb14a7436d4eda2a948c1b39b4
pc9839a-lgtm <pc9839a@naver.com>
2026-04-13 21:08:10 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbw0Jt8txNueQOuSoZXmHCkaS0dqTBcgVaYEgr8-VYELef2hTJ5tKR2qipJALo-O6Zw/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxoJNXHBDSXf32M7x89hVurC75kw8-I3-DfXoXcbflhuV4lpik8oSU0R2mu1jDaVBM/exec',
   useMockOnly: false,
   submitTimeout: 30000
 };
