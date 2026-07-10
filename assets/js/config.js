8f460744dfbd1f4740626df65404c0aaf4beae2d
pc9839a-lgtm <pc9839a@naver.com>
2026-07-10 13:47:12 +0900

    Trigger Pages deploy for updated blog index

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 window.APP_CONFIG = {
   // 구글시트 API는 일정, 후기, FAQ, 선실 등 운영 데이터용으로 사용합니다.
   // 블로그/콘텐츠 발행은 GitHub 정적 파일 기반으로 관리합니다.
+  // deploy check: 2026-07-10T13:40:00+09:00
   apiUrl: 'https://script.google.com/macros/s/AKfycbwcgILUioi3I3ndTIrnlZ9KtdN-YxpwCQaLEcradUc1vDZfsa-jSwllYfSdOju4vj8/exec',
   useMockOnly: false,
   submitTimeout: 30000
