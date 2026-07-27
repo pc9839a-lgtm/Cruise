c3989aa7c591713e272e7c2f06e5998604c3ffbe
pc9839a-lgtm <pc9839a@naver.com>
2026-07-27 17:43:12 +0900

    Serve sitemap XML through Pages Function

---
+++ functions/sitemap.xml.js
@@ -0,0 +1,26 @@
+const XML_HEADERS = {
+  'Content-Type': 'application/xml; charset=UTF-8',
+  'Cache-Control': 'public, max-age=0, must-revalidate, no-transform',
+  'X-Content-Type-Options': 'nosniff',
+  'X-Robots-Tag': 'all'
+};
+
+export async function onRequest({ request, env }) {
+  const assetResponse = await env.ASSETS.fetch(request);
+
+  if (!assetResponse.ok) {
+    return new Response('Sitemap asset unavailable\n', {
+      status: 503,
+      headers: {
+        'Content-Type': 'text/plain; charset=UTF-8',
+        'Cache-Control': 'no-store',
+        'X-Content-Type-Options': 'nosniff'
+      }
+    });
+  }
+
+  return new Response(assetResponse.body, {
+    status: 200,
+    headers: XML_HEADERS
+  });
+}
