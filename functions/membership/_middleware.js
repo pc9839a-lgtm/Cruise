6fecfba7841982c3c5b115539fac4bd8e43179a1
pc9839a-lgtm <pc9839a@naver.com>
2026-08-31 01:27:57 +0900

    Bump membership landing renderer cache

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -1,5 +1,5 @@
 const SURVEY_SCRIPT = '<script src="/membership/membership-entry-survey-v1.js?v=20260830-14"></script>';
-const FINAL_RENDERER = '<script src="/membership/membership-polish-v9.js?v=20260831-3"></script>';
+const FINAL_RENDERER = '<script src="/membership/membership-polish-v9.js?v=20260831-4"></script>';
 
 class MembershipScriptInjector {
   element(element) {
