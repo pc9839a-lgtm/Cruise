7c0563258f62f968e2e79f16ea6d4508bc76be79
pc9839a-lgtm <pc9839a@naver.com>
2026-08-30 15:15:23 +0900

    feat: inject entry survey before membership landing

---
+++ functions/membership/_middleware.js
@@ -0,0 +1,23 @@
+const SURVEY_SCRIPT = '<script src="/membership/membership-entry-survey-v1.js?v=20260830-1"></script>';
+
+class MembershipSurveyInjector {
+  element(element) {
+    element.prepend(SURVEY_SCRIPT, { html: true });
+  }
+}
+
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const response = await context.next();
+  const contentType = response.headers.get('content-type') || '';
+
+  const isMembershipLanding =
+    (url.pathname === '/membership/' || url.pathname === '/membership/index.html') &&
+    contentType.includes('text/html');
+
+  if (!isMembershipLanding) return response;
+
+  return new HTMLRewriter()
+    .on('body', new MembershipSurveyInjector())
+    .transform(response);
+}
