dc8b9cd79d72a0cb2d8161f6381933ed862a178c
pc9839a-lgtm <pc9839a@naver.com>
2026-08-30 16:33:38 +0900

    Bump membership survey cache version

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -1,4 +1,4 @@
-const SURVEY_SCRIPT = '<script src="/membership/membership-entry-survey-v1.js?v=20260830-8"></script>';
+const SURVEY_SCRIPT = '<script src="/membership/membership-entry-survey-v1.js?v=20260830-9"></script>';
 
 class MembershipSurveyInjector {
   element(element) {
