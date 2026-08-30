7bfde77f805d5577e9067656c803d9a7b73a5c8c
pc9839a-lgtm <pc9839a@naver.com>
2026-08-31 00:20:41 +0900

    Load only final membership renderer

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -1,35 +1,9 @@
 const SURVEY_SCRIPT = '<script src="/membership/membership-entry-survey-v1.js?v=20260830-14"></script>';
-const CONVERSION_SCRIPT = '<script src="/membership/membership-conversion-v3.js?v=20260830-8"></script>';
-const POLISH_SCRIPT = '<script src="/membership/membership-polish-v9.js?v=20260830-2"></script>';
-const FINAL_PATCH = `<script>
-(function(){
-  function patchMembershipBottom(){
-    var image=document.querySelector('#travel-desire .pmx-cruise-visual img');
-    if(image){
-      image.src='https://images.unsplash.com/photo-1688269910705-2d2a9d943a95?auto=format&fit=crop&q=85&w=1800';
-      image.alt='크루즈 선상 수영장과 바다 풍경';
-      image.style.objectPosition='center 52%';
-    }
-
-    ['join-faq','ig8-rule','ig8-final'].forEach(function(id){
-      var el=document.getElementById(id);
-      if(el) el.remove();
-    });
-
-    var oldCustom=document.getElementById('pmx-bottom-cta');
-    if(oldCustom) oldCustom.remove();
-  }
-  function run(){
-    setTimeout(patchMembershipBottom,140);
-    setTimeout(patchMembershipBottom,700);
-  }
-  if(document.readyState==='loading') document.addEventListener('DOMContentLoaded',run,{once:true}); else run();
-})();
-</script>`;
+const FINAL_RENDERER = '<script src="/membership/membership-polish-v9.js?v=20260830-5"></script>';
 
 class MembershipScriptInjector {
   element(element) {
-    element.prepend(SURVEY_SCRIPT + CONVERSION_SCRIPT + POLISH_SCRIPT + FINAL_PATCH, { html: true });
+    element.prepend(SURVEY_SCRIPT + FINAL_RENDERER, { html: true });
   }
 }
 
