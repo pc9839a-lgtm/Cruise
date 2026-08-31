8405243b5abbfa0a44dc0f03537b4f0d8d3f7a0a
pc9839a-lgtm <pc9839a@naver.com>
2026-08-31 12:45:57 +0900

    Load price bridge on membership page

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -1,3 +1,13 @@
 export async function onRequest(context) {
-  return context.next();
+  const response = await context.next();
+  const type = response.headers.get('content-type') || '';
+  if (!type.includes('text/html')) return response;
+
+  return new HTMLRewriter()
+    .on('body', {
+      element(element) {
+        element.append('<script src="/membership/membership-price-bridge-v1.js?v=20260831-1"></script>', { html: true });
+      }
+    })
+    .transform(response);
 }
