32fa5bb75f0efe2ad5f97298728869052ba30294
pc9839a-lgtm <pc9839a@naver.com>
2026-08-31 13:19:33 +0900

    Bust cache for restored nav and looping motion

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
   return new HTMLRewriter()
     .on('body', {
       element(element) {
-        element.append('<script src="/membership/membership-price-bridge-v1.js?v=20260831-4"></script><script src="/membership/membership-adflow-1-4-v1.js?v=20260831-4"></script>', { html: true });
+        element.append('<script src="/membership/membership-price-bridge-v1.js?v=20260831-4"></script><script src="/membership/membership-adflow-1-4-v1.js?v=20260831-5"></script>', { html: true });
       }
     })
     .transform(response);
