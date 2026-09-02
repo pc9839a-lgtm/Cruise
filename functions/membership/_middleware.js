ad6a445874f52872665a9194b68a3b0657f8bd36
pc9839a-lgtm <pc9839a@naver.com>
2026-09-03 00:07:11 +0900

    Bust cache for corrected Classic receipt math

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -49,7 +49,7 @@ export async function onRequest(context) {
           '<script defer src="/membership/membership-section4-moving-hotel-v1.js?v=20260902-story2"></script>' +
           '<script defer src="/membership/membership-impact-flow-v1.js?v=20260902-story2"></script>' +
           '<script defer src="/membership/membership-section6-price-barrier-v1.js?v=20260902-concise4"></script>' +
-          '<script defer src="/membership/membership-section7-8-price-proof-v1.js?v=20260903-realcost8"></script>' +
+          '<script defer src="/membership/membership-section7-8-price-proof-v1.js?v=20260903-classicmath9"></script>' +
           '<script defer src="/membership/membership-section9-10-cost-use-v1.js?v=20260902-storyclean6"></script>' +
           '<script defer src="/membership/membership-section11-12-guide-transition-v1.js?v=20260902-storyclean6"></script>' +
           '<script defer src="/membership/membership-section13-14-points-v1.js?v=20260902-flowclean5"></script>' +
