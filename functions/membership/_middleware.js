9f5b564bff1db99ea11119bbd2eb26cbd4d62820
pc9839a-lgtm <pc9839a@naver.com>
2026-09-02 22:50:45 +0900

    Bust cache for actual Western Mediterranean receipt examples

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -84,13 +84,13 @@ export async function onRequest(context) {
           '<script defer src="/membership/membership-section3-port-v1.js?v=20260902-story2"></script>' +
           '<script defer src="/membership/membership-section4-moving-hotel-v1.js?v=20260902-story2"></script>' +
           '<script defer src="/membership/membership-impact-flow-v1.js?v=20260902-story2"></script>' +
-          '<script defer src="/membership/membership-section6-price-barrier-v1.js?v=20260902-medreceipt1"></script>' +
-          '<script defer src="/membership/membership-section7-8-price-proof-v1.js?v=20260902-medreceipt1"></script>' +
-          '<script defer src="/membership/membership-section9-10-cost-use-v1.js?v=20260902-medreceipt1"></script>' +
+          '<script defer src="/membership/membership-section6-price-barrier-v1.js?v=20260902-receipt2"></script>' +
+          '<script defer src="/membership/membership-section7-8-price-proof-v1.js?v=20260902-receipt2"></script>' +
+          '<script defer src="/membership/membership-section9-10-cost-use-v1.js?v=20260902-receipt2"></script>' +
           '<script defer src="/membership/membership-section11-12-guide-transition-v1.js?v=20260902-story2"></script>' +
-          '<script defer src="/membership/membership-section13-14-points-v1.js?v=20260902-story2"></script>' +
-          '<script defer src="/membership/membership-section15-16-point-payment-v1.js?v=20260902-story2"></script>' +
-          '<script defer src="/membership/membership-section17-18-calculator-fit-v1.js?v=20260902-story2"></script>' +
+          '<script defer src="/membership/membership-section13-14-points-v1.js?v=20260902-receipt2"></script>' +
+          '<script defer src="/membership/membership-section15-16-point-payment-v1.js?v=20260902-receipt2"></script>' +
+          '<script defer src="/membership/membership-section17-18-calculator-fit-v1.js?v=20260902-receipt2"></script>' +
           '<script defer src="/membership/membership-section19-20-plan-choice-v1.js?v=20260902-story2"></script>' +
           '<script defer src="/membership/membership-section21-22-terms-final-v1.js?v=20260902-story2"></script>' +
           '<script defer src="/membership/membership-hero-restore-v1.js?v=20260902-subscription-restore"></script>' +
