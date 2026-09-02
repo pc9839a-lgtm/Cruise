74bde91928151318f6bfe638f004de2b79525c7a
pc9839a-lgtm <pc9839a@naver.com>
2026-09-02 10:44:12 +0900

    membership: load sections 13 and 14 rebuild

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -18,6 +18,7 @@ export async function onRequest(context) {
   const section78Style = '<link rel="stylesheet" href="/membership/membership-section7-8-price-proof-v1.css?v=20260902-1">';
   const section910Style = '<link rel="stylesheet" href="/membership/membership-section9-10-cost-use-v1.css?v=20260902-1">';
   const section1112Style = '<link rel="stylesheet" href="/membership/membership-section11-12-guide-transition-v1.css?v=20260902-1">';
+  const section1314Style = '<link rel="stylesheet" href="/membership/membership-section13-14-points-v1.css?v=20260902-1">';
   const surveyMobileStyle = '<link rel="stylesheet" href="/membership/membership-survey-mobile-v3.css?v=20260901-1">';
 
   const mobileStyles =
@@ -34,6 +35,7 @@ export async function onRequest(context) {
     section78Style +
     section910Style +
     section1112Style +
+    section1314Style +
     surveyMobileStyle;
 
   const desktopStyles =
@@ -52,7 +54,8 @@ export async function onRequest(context) {
     section6Style +
     section78Style +
     section910Style +
-    section1112Style;
+    section1112Style +
+    section1314Style;
 
   const extraStyles = isMobile ? mobileStyles : desktopStyles;
 
@@ -94,6 +97,7 @@ export async function onRequest(context) {
           '<script defer src="/membership/membership-section7-8-price-proof-v1.js?v=20260902-1"></script>' +
           '<script defer src="/membership/membership-section9-10-cost-use-v1.js?v=20260902-1"></script>' +
           '<script defer src="/membership/membership-section11-12-guide-transition-v1.js?v=20260902-1"></script>' +
+          '<script defer src="/membership/membership-section13-14-points-v1.js?v=20260902-1"></script>' +
           (isMobile ? '<script defer src="/membership/membership-mobile-motion-v2.js?v=20260901-opt1"></script>' : '') +
           '<script defer src="/membership/membership-impact-motion-v1.js?v=20260901-opt1"></script>',
           { html: true }
