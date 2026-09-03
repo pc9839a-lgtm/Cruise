86e3a81c5054a88a873f56550a097842e50472fe
pc9839a-lgtm <pc9839a@naver.com>
2026-09-03 13:48:58 +0900

    Deploy refined calculator layout

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -55,7 +55,7 @@ export async function onRequest(context) {
           '<script defer src="/membership/membership-section11-12-guide-transition-v1.js?v=20260903-flow30"></script>' +
           '<script defer src="/membership/membership-section13-14-points-v1.js?v=20260903-flow23"></script>' +
           '<script defer src="/membership/membership-section15-16-point-payment-v1.js?v=20260903-flow24"></script>' +
-          '<script defer src="/membership/membership-section17-18-calculator-fit-v1.js?v=20260903-flow24"></script>' +
+          '<script defer src="/membership/membership-section17-18-calculator-fit-v1.js?v=20260903-flow31"></script>' +
           '<script defer src="/membership/membership-section19-20-plan-choice-v1.js?v=20260903-flow25"></script>' +
           '<script defer src="/membership/membership-section21-22-terms-final-v1.js?v=20260903-flow25"></script>' +
           '<script defer src="/membership/membership-hero-restore-v1.js?v=20260902-subscription-restore"></script>' +
