ddf4263376661591f45dbb5b527fb8f5652669d0
pc9839a-lgtm <pc9839a@naver.com>
2026-09-11 13:28:14 +0900

    Bump section 08 runtime cache keys

--- functions/membership/_middleware.js
+++ functions/membership/_middleware.js
@@ -196,8 +196,8 @@ export async function onRequest(context) {
           '<script defer src="/membership/membership-section3-port-v1.js?v=20260911-v3s05-ingroup1"></script>' +
           '<script defer src="/membership/membership-section4-moving-hotel-v1.js?v=20260911-v3s06"></script>' +
           '<script defer src="/membership/membership-impact-flow-v1.js?v=20260911-v3s07"></script>' +
-          '<script defer src="/membership/membership-section6-price-barrier-v1.js?v=20260903-flow23"></script>' +
-          '<script defer src="/membership/membership-section7-8-price-proof-v1.js?v=20260911-v3s07-compat1"></script>' +
+          '<script defer src="/membership/membership-section6-price-barrier-v1.js?v=20260911-v3s08"></script>' +
+          '<script defer src="/membership/membership-section7-8-price-proof-v1.js?v=20260911-v3s08-compat1"></script>' +
           '<script defer src="/membership/membership-section9-10-cost-use-v1.js?v=20260903-flow29"></script>' +
           '<script defer src="/membership/membership-section11-12-guide-transition-v1.js?v=20260903-flow30"></script>' +
           '<script defer src="/membership/membership-cruise-example-images-v1.js?v=20260903-cards45"></script>' +
